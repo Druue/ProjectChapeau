48baa3430e43d11aab0f2a9a1d77ac91b75771fd
Marianne Uhl <611034@student.inholland.nl>
2018-05-09 16:59:50 +0200

    test marianne

--- ProjectChapeau/ProjectChapeau/Form1.Designer.cs
+++ ProjectChapeau/ProjectChapeau/Form1.Designer.cs
@@ -28,21 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MarianneWasHere = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // MarianneWasHere
+            // 
+            this.MarianneWasHere.AutoSize = true;
+            this.MarianneWasHere.Location = new System.Drawing.Point(469, 171);
+            this.MarianneWasHere.Name = "MarianneWasHere";
+            this.MarianneWasHere.Size = new System.Drawing.Size(148, 21);
+            this.MarianneWasHere.TabIndex = 0;
+            this.MarianneWasHere.Text = "MarianneWasHere";
+            this.MarianneWasHere.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MarianneWasHere);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox MarianneWasHere;
     }
 }
 
