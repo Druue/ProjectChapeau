e86b83c62902dec565e8fb4ee086d199889db538
Marianne Uhl <611034@student.inholland.nl>
2018-05-22 11:41:18 +0200

    copy from machelle

--- ProjectChapeau/ChapeauModel/Class1.cs
+++ ProjectChapeau/ChapeauModel/Class1.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace ChapeauModel
 {
-    public class Class1
-    {
-    }
+        public class ChapeauModel
+        {
+
+            public class TableTop
+            {
+
+                int TableId;
+                int Seats;
+                string TableStatus;
+
+                public TableTop()
+                {
+
+                }
+
+                public TableTop(int id, int seats, string status)
+                {
+                    this.TableId = id;
+                    this.Seats = seats;
+                    this.TableStatus = status;
+                }
+
+                public int getSeats()
+                {
+                    return Seats;
+                }
+
+                public string getTableStatus()
+                {
+                    return TableStatus;
+                }
+
+                public int getTableId()
+                {
+                    return TableId;
+                }
+            }
+        }
 }
