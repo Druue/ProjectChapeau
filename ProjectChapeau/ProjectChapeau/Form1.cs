57da3be7f9f28ef16bfff601831ad1aa710b97d2
Marianne Uhl <611034@student.inholland.nl>
2018-05-23 12:33:57 +0200

    changes that did not work

--- ProjectChapeau/ProjectChapeau/Form1.cs
+++ ProjectChapeau/ProjectChapeau/Form1.cs
@@ -28,7 +28,10 @@ namespace ProjectChapeau
             login_panel.TabIndex = 0;
             login_panel.Location = new System.Drawing.Point(0, 0);
             login_panel.BackColor = Color.Orange;
+            //login_panel.BackgroundImage = Image.FromFile(System.Environment.GetFolderPath
+              //  (System.Environment.SpecialFolder.Personal) + @"\Image.gif");
 
+            
             //this allows the panel to get the size of the Form, even when resized by the user
             login_panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom)
             | AnchorStyles.Left)
@@ -36,7 +39,10 @@ namespace ProjectChapeau
             login_panel.AutoSize = true;
 
             //adding the login_panel to the Controls in the Form
+            login_panel.Controls.Clear();
             Controls.Add(login_panel);
+
+            login_panel.Controls.Add(ChapeauUI.);
 
 
             //LOGIN BUTTON 
@@ -54,9 +60,49 @@ namespace ProjectChapeau
             button_login.FlatAppearance.BorderSize = 1;
 
             button_login.Location = new Point((login_panel.Width / 2 - loginButtonWidth), login_panel.Height / 2);
+            button_login.Click += new System.EventHandler(button_login_Click);            
             login_panel.Controls.Add(button_login);
 
+        }
+      
 
+        void button_login_Click(object sender,EventArgs e)
+        {
+            Panel manager_panel = new Panel();
+            manager_panel.Name = "managerPanel";
+            manager_panel.Size = new Size(panelWidth, panelHeight);
+            manager_panel.TabIndex = 0;
+            manager_panel.Location = new System.Drawing.Point(0, 0);
+            manager_panel.BackColor = Color.Orange;
+            //login_panel.BackgroundImage = Image.FromFile(System.Environment.GetFolderPath
+            //  (System.Environment.SpecialFolder.Personal) + @"\Image.gif");
+
+
+            //this allows the panel to get the size of the Form, even when resized by the user
+            manager_panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom)
+            | AnchorStyles.Left)
+            | AnchorStyles.Right)));
+            manager_panel.AutoSize = true;
+
+            //adding the login_panel to the Controls in the Form
+            Controls.Add(manager_panel);
+
+            //button for overview menu
+            Button button_menu = new Button();
+            int menuButtonWidth = 160;
+            int menuButtonHeight = 30;
+
+            button_menu.Text = "Menu";
+            button_menu.Width = menuButtonWidth;
+            button_menu.Height = menuButtonHeight;
+            button_menu.ForeColor = Color.White;
+            button_menu.Font = new Font("Arial", 10, FontStyle.Bold);
+            button_menu.FlatStyle = FlatStyle.Flat;
+            button_menu.FlatAppearance.BorderColor = Color.White;
+            button_menu.FlatAppearance.BorderSize = 1;
+
+            button_menu.Location = new Point((manager_panel.Width / 5 - menuButtonWidth), manager_panel.Height / 3);
+            manager_panel.Controls.Add(button_menu);
         }
     }
 }
