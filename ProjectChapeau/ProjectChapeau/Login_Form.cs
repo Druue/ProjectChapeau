5199d78b3a8e34e69ccc121f48582098eee779f0
MachelleBa <613954@student.inholland.nl>
2018-05-23 13:42:19 +0200

    Stuff we need to merge

--- ProjectChapeau/ProjectChapeau/Login_Form.cs
+++ ProjectChapeau/ProjectChapeau/Login_Form.cs
@@ -7,101 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using System.Drawing;
-using Chapeau_UI;
+using System.Drawing;
 
 namespace ProjectChapeau
 {
-    
 
-    public partial class Form1 : Form
+
+    public partial class LoginForm :Form
     {
         public int panelWidth = 1400; //please use these sizes for the form
         public int panelHeight = 600;
 
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) //filled in by Machelle
-        
-        private void btn_TestOverview_Click(object sender, EventArgs e)//elizabeths
+        private void LoginForm_Load(object sender, EventArgs e) //filled in by Machelle
         {
-            // Test form
-            Form orderForm = new Form();
-            orderForm.Text = "Order review form";
-            orderForm.Location = new Point(50, 10);
-            orderForm.Size = new Size(800, 600);
-            orderForm.Controls.Add(ChapeauUI.showOrders());
-            //orderForm.Controls.Add(ChapeauUI.showOrders());
-            orderForm.ShowDialog();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            // Test panel
-            Panel panelTest = new Panel();
-            panelTest.Name = "pnlMainDisplay";
-            panelTest.Size = new Size(panelWidth, panelHeight);
-            panelTest.TabIndex = 0;
-            panelTest.Location = new System.Drawing.Point(0, 0);
-
-            //this.Form1_Load.login_panel.clear();
-            //this.login_panel = new Panel();
-            //login_panel.Name = "loginPanel";
-            
-            // Test button
-            Button testButton = new Button();
-            testButton.Text = "Show order";
-            testButton.Name = "btnOrder";
-            testButton.Click += new EventHandler(btn_TestOverview_Click);
-
-            Controls.Add(testButton);
-            Controls.Add(panelTest);
-
-
-
-
-            /*
-            Panel login_panel = new Panel();
-            login_panel.Name = "loginPanel";
+            //this panel has been created in Form1.Designer, at the top, to make it global
             login_panel.Size = new Size(panelWidth, panelHeight);
             login_panel.TabIndex = 0;
             login_panel.Location = new System.Drawing.Point(0, 0);
             login_panel.BackColor = Color.Orange;
 
-
-            //this allows the panel to get the size of the Form, even when resized by the user
-            login_panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((AnchorStyles.Top | AnchorStyles.Bottom)
+            //sizing stuff login_panel
+            login_panel.Anchor = ((System.Windows.Forms.AnchorStyles)
+            ((((AnchorStyles.Top | AnchorStyles.Bottom)
             | AnchorStyles.Left)
             | AnchorStyles.Right)));
             login_panel.AutoSize = true;
-
-            //adding the login_panel to the Controls in the Form
+            
             Controls.Add(login_panel);
 
 
             //LOGIN TO BEGIN BUTTON 
-            Button button_login_to_begin = new Button();
             int loginButtonWidth = 160;
             int loginButtonHeight = 30;
+            Button button_login_to_begin = new Button();
 
-            button_login_to_begin.Text = "Log in to begin";
+            //login to begin button set up
             button_login_to_begin.Width = loginButtonWidth;
             button_login_to_begin.Height = loginButtonHeight;
+            button_login_to_begin.Location = 
+            new Point((login_panel.Width / 2 - loginButtonWidth), login_panel.Height / 2);
+            
+
+            //login button styling + text
             button_login_to_begin.ForeColor = Color.White;
             button_login_to_begin.Font = new Font("Arial", 10, FontStyle.Bold);
             button_login_to_begin.FlatStyle = FlatStyle.Flat;
             button_login_to_begin.FlatAppearance.BorderColor = Color.White;
             button_login_to_begin.FlatAppearance.BorderSize = 1;
+            button_login_to_begin.Text = "Log in to begin";
 
+            //login button eventhandler
             button_login_to_begin.Click += new EventHandler(button_login_to_begin_Click);
 
-
-
-            button_login_to_begin.Location = new Point((login_panel.Width / 2 - loginButtonWidth), login_panel.Height / 2);
+            //adding it to the login_panel
             login_panel.Controls.Add(button_login_to_begin);
+
 
 
         }
@@ -127,6 +92,7 @@ namespace ProjectChapeau
 
             //-----------------------------------------------------------------------------------------GROUPBOX FOR LOGIN CONTROLS ---------------------------------------------------------------------------------------------------------------------
 
+            //group box that contains the log in textboxes and button
             GroupBox login_controls_grpbox = new GroupBox();
             login_controls_grpbox.Location = new System.Drawing.Point(100, 100);
             login_controls_grpbox.Size = new Size(login_panel.Width / 3, login_panel.Height / 3);
@@ -173,10 +139,6 @@ namespace ProjectChapeau
             button_login.FlatStyle = FlatStyle.Flat;
             button_login.FlatAppearance.BorderColor = Color.White;
             button_login.FlatAppearance.BorderSize = 1;
-   
-            button_login.Location = new Point((login_panel.Width / 2 - loginButtonWidth), login_panel.Height / 2);
-            login_panel.Controls.Add(button_login);
-            */
 
             button_login.Click += new EventHandler(button_login_Click);
 
