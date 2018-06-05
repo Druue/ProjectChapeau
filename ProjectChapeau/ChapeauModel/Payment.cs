adf9a2282a4d33f84476f81890decdd8b6f71451
Elizabeth <elizabeth.erickson21@gmail.com>
2018-06-05 11:45:59 +0200

    removed is payed from payment.cs
    is payed was no longer needed. removed it

--- ProjectChapeau/ChapeauModel/Payment.cs
+++ ProjectChapeau/ChapeauModel/Payment.cs
@@ -9,24 +9,17 @@ namespace Chapeau_Model
         public double Vat { get; set; }
         public double Tip { get; set; }
         public double TotalPayment { get; set; }
-        //public bool IsPayed { get; set; } not sure if keeping
         public string Comments { get; set; }
         public DateTime TimePayed { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
         public List<OrderItems> OrderItems { get; set; }
 
-        //public Payment() Not sure if empty constructor is needed
-        //{
-
-        //}
-
-        public Payment(int employeeId , int vat, double tip, double totalPayment, bool isPayed, string comments, PaymentMethod paymentMethod, OrderItems orderItems)
+        public Payment(int employeeId , int vat, double tip, double totalPayment, string comments, PaymentMethod paymentMethod, OrderItems orderItems)
         {
             EmployeeId = employeeId;
             Vat = vat;
             Tip = tip;
             TotalPayment = totalPayment;
-            //IsPayed = isPayed;
             Comments = comments;
             TimePayed = DateTime.Now;
             PaymentMethod = paymentMethod;
