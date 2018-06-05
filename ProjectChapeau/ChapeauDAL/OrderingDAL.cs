9f5a58169a19c18b567df0bed17c21683098a0b2
Bueno Montoya, Henry <610463@student.inholland.nl>
2018-06-05 11:47:42 +0200

    Added new Ordering files

--- ProjectChapeau/ChapeauDAL/OrderingDAL.cs
+++ ProjectChapeau/ChapeauDAL/OrderingDAL.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
-namespace ChapeauTest
+namespace ChapeauTestV2
 {
-    class OrderingDAL
+    public class OrderingDAL
     {
         private SqlConnection OpenConnectionDB()
         {
@@ -39,20 +40,75 @@ namespace ChapeauTest
         {
             SqlConnection connection = OpenConnectionDB();
             List<OrderingModel.Item> MenuItemsList = new List<OrderingModel.Item>();
-            string sqlQuery = "SELECT ItemId, ItemName, Price, Vat, stock, MenuType FROM Menu";
+            string sqlQuery = "SELECT ItemId, ItemName, Price, stock, MenuType FROM Menu";
             SqlCommand command = new SqlCommand(sqlQuery, connection);
             SqlDataReader reader = command.ExecuteReader();
 
             while (reader.Read())
             {
-                OrderingModel.Item Item = new OrderingModel.Item((int)reader["ItemId"], (int)reader["stock"], 
-                    (string)reader["name"], (double)reader["Price"], (double)reader["Vat"]); 
+                OrderingModel.Item Item = new OrderingModel.Item((int)reader["ItemId"], (int)reader["stock"],
+                    (string)reader["ItemName"], (decimal)reader["Price"], (string)reader["MenuType"]);
+                MenuItemsList.Add(Item);
             }
             reader.Close();
             connection.Close();
             return MenuItemsList;
         }
 
+        public void DB_InsertOrder(List<OrderingModel.Item> OrderItemsList)
+        {
+            try
+            {
+                string time = DateTime.Now.ToShortTimeString().ToString();
+                string date = DateTime.Now.ToShortDateString();
+                string datetime = time + " " + date;
+                // wquery to select from specific columns
 
+                using (SqlCommand cmd =
+                    new SqlCommand("INSERT INTO Orders VALUES(" +
+                        "@OrderId, @OrderTime)", OpenConnectionDB()))
+                {
+                    cmd.Parameters.AddWithValue("@OrderId", "mysql_insert_id() + 1");
+                    cmd.Parameters.AddWithValue("@OrderTime", datetime);
+
+                    int rows = cmd.ExecuteNonQuery();
+
+                }
+
+                using (SqlCommand cmd =
+                    new SqlCommand("INSERT INTO OrderItems VALUES(" +
+                        "@OrderId, @ItemId, @Comment)", OpenConnectionDB()))
+                {
+
+                    foreach (OrderingModel.Item item in OrderItemsList)
+                    {
+                        cmd.Parameters.AddWithValue("@OrderId", "mysql_insert_id() + 1");
+                        cmd.Parameters.AddWithValue("@ItemId", item.itemID);
+                        cmd.Parameters.AddWithValue("@Comment", item.comment);
+
+                    }
+
+                    int rows = cmd.ExecuteNonQuery();
+                
+                }
+                
+            foreach (OrderingModel.Item item in OrderItemsList){
+
+            using (SqlCommand cmd =
+                   new SqlCommand("UPDATE Menu SET stock = @stock WHERE ID = " + item.itemID, OpenConnectionDB()))
+                    {
+                        cmd.Parameters.AddWithValue("@stock", "[stock - " + item.quantity + "]");
+                        int rows = cmd.ExecuteNonQuery();
+                    }
+                }               
+                OpenConnectionDB().Close();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(string.Format("An error occurred: {0}", ex.Message));
+            }
+
+        }
     }
 }
--- ProjectChapeau/ChapeauModel/OrderingModel.cs
+++ ProjectChapeau/ChapeauModel/OrderingModel.cs
@@ -3,45 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace ChapeauTest
+namespace ChapeauTestV2
 {
     public class OrderingModel
     {
+        public class Order
+        {
+            public int OrderID, TableID, EmployeeID;
+            public double order_price;
+            public bool order_PaidStatus;
+            public List<Item> order_items;
 
-            public class Order
+            public Order()
             {
-                public int OrderID, TableID, EmployeeID;
-                public double order_price, order_vat;
-                public bool order_PaidStatus;
-                
-                public Order(int OrderID, int TableID, int EmployeeID, double order_price, double order_vat)
-                {
-                    this.OrderID = OrderID;
-                    this.TableID = TableID;
-                    this.EmployeeID = EmployeeID;
-                    this.order_price = order_price;
-                    this.order_vat = order_vat;
-                }               
+
             }
 
-            public class Item
+            public Order(int OrderID, int TableID, int EmployeeID, double order_price)
             {
-                public int itemID, stock;
-                public string name;
-                public double item_price, item_vat;
-            
-
-                public Item(int itemID, int stock, string name, double item_price, double item_vat)
-                {
-                    this.itemID = itemID;
-                    this.stock = stock;
-                    this.name = name;               
-                    this.item_price = item_price;
-                    this.item_vat = item_vat;
-
-                }
+                this.OrderID = OrderID;
+                this.TableID = TableID;
+                this.EmployeeID = EmployeeID;
+                this.order_price = order_price;
+            }
         }
-        
+
+        public class Item
+        {
+            public int itemID;
+            public string name;
+            public decimal item_price;
+            public string comment;
+            public int quantity;
+            public string MenuType;
+
+            public Item()
+            {
+
+            }
+
+            public Item(int itemID, int stock, string name, decimal item_price, string MenuType)
+            {
+                this.itemID = itemID;
+                this.name = name;
+                this.item_price = item_price;
+                this.MenuType = MenuType;
+
+            }
+        }
+    }
+
+
+    public class DataGridViewNumericUpDownCell : DataGridViewTextBoxCell
+    {
+        public DataGridViewNumericUpDownCell()
+        {
+
+        }
     }
 }
+
