badb9b78a70af71832bb881ce0a719b2db9f829b
MachelleBa <613954@student.inholland.nl>
2018-06-17 16:04:51 +0200

    Tableoverview now has the additional buttons next to the table that show more info on status. They don't funtion fully yet

--- ProjectChapeau/ProjectChapeau/RestaurantOverview_Form.cs
+++ ProjectChapeau/ProjectChapeau/RestaurantOverview_Form.cs
@@ -25,20 +25,26 @@ namespace ProjectChapeau
 
             lbl_loggedinEmployee.ForeColor = Color.Black;
             lbl_loggedinEmployee.Text = employee.Firstname + " " + employee.Lastname;
-            lbl_loggedinEmployee.Font =new Font(lbl_loggedinEmployee.Font, FontStyle.Bold);
+            lbl_loggedinEmployee.Font = new Font(lbl_loggedinEmployee.Font, FontStyle.Bold);
             pnl_topbar.Controls.Add(lbl_loggedinEmployee);
 
             foreach (TableTop table in AllTables)
             {
                 TableButton tablebutton = new TableButton(table, this);
+                StatusButton statusButton = new StatusButton(table, 1);
+                StatusButton satTimeButton = new StatusButton(table, 2);
 
                 if (table.GetTableId() % 2 == 0)
                 {
                     this.flowlaypnl_table_overview_even.Controls.Add(tablebutton); //add the even buttons here (2,4,6,8,10)
+                    this.flowlaypnl_statusEven.Controls.Add(statusButton);
+                    this.flowlaypnl_statusEven.Controls.Add(satTimeButton);
                 }
                 else
                 {
                     this.flowlaypnl_table_overview_uneven.Controls.Add(tablebutton); //add the uneven buttons here (1,3,5,7,9)
+                    this.flowlaypnl_statusUneven.Controls.Add(statusButton);
+                    this.flowlaypnl_statusUneven.Controls.Add(satTimeButton);
                 }
             }
         }
@@ -48,11 +54,11 @@ namespace ProjectChapeau
 
             public TableButton(TableTop table, RestaurantOverview_Form form)
             {
-                this.Size = new Size(80, 80);
+                this.Size = new Size(60, 60);
                 this.Font = new Font("Arial", 12, FontStyle.Bold);
-                this.Text = table.GetTableId().ToString();                
+                this.Text = table.GetTableId().ToString();
                 this.Click += new EventHandler((s, ev) => TableButton_Click(s, ev, table, form));
-                this.Margin = new Padding(4,14, 4, 4);
+                this.Margin = new Padding(0, 25, 0, 0);
 
                 if (table.GetTableStatus() == TableStatus.Available)
                 {
@@ -73,20 +79,20 @@ namespace ProjectChapeau
             public void TableButton_Click(object sender, EventArgs e, TableTop table, RestaurantOverview_Form form)
             {
 
-              form.Hide();
+                
 
                 if (table.GetTableStatus() == TableStatus.Available)
                 {
                     OrderingForm ordering = new OrderingForm(table);
-                    ordering.Show();
+                    ordering.ShowDialog();
                 }
                 else
                 {
                     OccupiedTableForm occupied = new OccupiedTableForm(table.GetTableId());
-                    occupied.Show();
+                    occupied.ShowDialog();
                 }
-                
-                
+
+
 
 
 
@@ -97,6 +103,39 @@ namespace ProjectChapeau
 
             }
         }
+
+        public class StatusButton : Button
+        {
+            public StatusButton(TableTop table, int position)
+            {
+                this.Size = new Size(70, 30);
+                this.Font = new Font("Arial", 8);
+                this.Text = table.GetTableStatus().ToString();
+
+                if (position % 2 != 0)
+                {
+                    this.Margin = new Padding(0, 25, 0, 0);
+
+                    if (table.GetTableStatus() == TableStatus.Available)
+                    {
+                        this.Text = " - ";
+                    }
+                    else if (table.GetTableStatus() == TableStatus.Occupied)
+                    {
+
+                    }
+                }
+                else
+                {
+                    this.Margin = new Padding(0, 0, 0, 0);
+                    this.Text = table.GetTableStatus().ToString();
+                }
+
+
+            }
+        }
+
+
 
         private void btn_logoff_Click(object sender, EventArgs e)
         {
