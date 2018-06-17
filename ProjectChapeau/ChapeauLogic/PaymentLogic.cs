7af2dae7e57aa7014735174c12a35eca6c0e01af
Sophie Ibbs <622294@student.inholland.nl>
2018-06-17 02:05:26 +0200

    payment form skeleton created tweaked some formatting in paymentlogic
    ALERT: THIS MIGHT BREAK SOMETHING - CREATE A BACKUP BEFORE PULLING THIS PLS

--- ProjectChapeau/ChapeauLogic/PaymentLogic.cs
+++ ProjectChapeau/ChapeauLogic/PaymentLogic.cs
@@ -15,16 +15,18 @@ namespace Chapeau_Logic
             int itemid = 1;
             int orderid = 1;
 
-            Payment payment = new Payment();
-            payment.EmployeeId = 1; //gotten from machelle's part
-            payment.TableId = 1; //Gotten from table overview?
-            payment.Tip = 0; //tip = user input from form
-            payment.Vat = 0;
-            payment.InitialPrice = 0;
-            payment.PaymentMethod = 0; //PaymentMethod = user input from form
-            payment.Comments = "";
+            Payment payment = new Payment
+            {
+                EmployeeId = 1, //gotten from machelle's part
+                TableId = 1, //Gotten from table overview?
+                Tip = 0, //tip = user input from form
+                Vat = 0,
+                InitialPrice = 0,
+                PaymentMethod = 0, //PaymentMethod = user input from form
+                Comments = ""
+            };
 
-            Chapeau_DAL.ChapeauDAL dal = new Chapeau_DAL.ChapeauDAL();
+            ChapeauDAL dal = new ChapeauDAL();
             List<OrderItems> orderItems = dal.OrderItemsDAO(itemid, orderid);
 
             foreach (OrderItems item in orderItems)
--- ProjectChapeau/ProjectChapeau/Payment_Form.Designer.cs
+++ ProjectChapeau/ProjectChapeau/Payment_Form.Designer.cs
@@ -31,12 +31,15 @@
             this.payMethod1 = new System.Windows.Forms.RadioButton();
             this.payMethod2 = new System.Windows.Forms.RadioButton();
             this.payMethod3 = new System.Windows.Forms.RadioButton();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.finishedPaymentBttn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // payMethod1
             // 
             this.payMethod1.AutoSize = true;
-            this.payMethod1.Location = new System.Drawing.Point(186, 52);
+            this.payMethod1.Location = new System.Drawing.Point(63, 52);
             this.payMethod1.Name = "payMethod1";
             this.payMethod1.Size = new System.Drawing.Size(107, 21);
             this.payMethod1.TabIndex = 0;
@@ -47,7 +50,7 @@
             // payMethod2
             // 
             this.payMethod2.AutoSize = true;
-            this.payMethod2.Location = new System.Drawing.Point(439, 52);
+            this.payMethod2.Location = new System.Drawing.Point(215, 52);
             this.payMethod2.Name = "payMethod2";
             this.payMethod2.Size = new System.Drawing.Size(107, 21);
             this.payMethod2.TabIndex = 1;
@@ -58,7 +61,7 @@
             // payMethod3
             // 
             this.payMethod3.AutoSize = true;
-            this.payMethod3.Location = new System.Drawing.Point(699, 52);
+            this.payMethod3.Location = new System.Drawing.Point(372, 52);
             this.payMethod3.Name = "payMethod3";
             this.payMethod3.Size = new System.Drawing.Size(107, 21);
             this.payMethod3.TabIndex = 2;
@@ -66,11 +69,38 @@
             this.payMethod3.Text = "payMethod3";
             this.payMethod3.UseVisualStyleBackColor = true;
             // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(157, 100);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(220, 269);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 439);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // finishedPaymentBttn
+            // 
+            this.finishedPaymentBttn.Location = new System.Drawing.Point(157, 484);
+            this.finishedPaymentBttn.Name = "finishedPaymentBttn";
+            this.finishedPaymentBttn.Size = new System.Drawing.Size(220, 23);
+            this.finishedPaymentBttn.TabIndex = 5;
+            this.finishedPaymentBttn.Text = "Finalise Payment";
+            this.finishedPaymentBttn.UseVisualStyleBackColor = true;
+            // 
             // Payment_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(547, 554);
+            this.Controls.Add(this.finishedPaymentBttn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.payMethod3);
             this.Controls.Add(this.payMethod2);
             this.Controls.Add(this.payMethod1);
@@ -88,5 +118,8 @@
         private System.Windows.Forms.RadioButton payMethod1;
         private System.Windows.Forms.RadioButton payMethod2;
         private System.Windows.Forms.RadioButton payMethod3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button finishedPaymentBttn;
     }
 }
